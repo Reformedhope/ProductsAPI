9e22007e3e1969bdf573d7e3f3360051c493c702
Stephanie Salzburg <steph_salzburg@hotmail.com>
2023-02-19 13:16:48 -0500

    Updated REst framework  with admine site + completed Post funcunalty  for product + updated Decimal setting in rest framework

--- products/views.py
+++ products/views.py
@@ -1,15 +1,23 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import ProductSerializer
 from .models import Products
 
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def products_list(request):
     
-    products = Products.objects.all()
+    if request.method == 'GET':
+        products = Products.objects.all()
+        serializer = ProductSerializer(products, many = True)
+        return Response(serializer.data)
+    elif request.method =='POST':
+         serializer = ProductSerializer(data=request.data)
+         if serializer.is_valid() == True:
+             serializer.save()
+             return Response(serializer.data, status=status.HTTP_201_CREATED)
+         else:
+             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    serializer = ProductSerializer(products, many = True)
-
-    return Response(serializer.data)
 
