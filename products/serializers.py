278875b99405b11b042f4ccfd969d4abd3d8a567
Stephanie Salzburg <steph_salzburg@hotmail.com>
2023-02-19 12:49:18 -0500

    Added products to the database via the URL interface

--- products/serializers.py
+++ products/serializers.py
@@ -2,6 +2,6 @@ from rest_framework import serializers
 from .models import Products
 
 class ProductSerializer(serializers.ModelSerializer):
-    class meta:
+    class Meta:
         model = Products
         fields = ['id', 'title','description', 'price','inventory_quantity']
